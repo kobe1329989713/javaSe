a965eb08ed192e35ed287ec5ef6c41db2bfc710f
刘坤 <kobe@liukundeMac.local>
2019-06-12 14:41:11 +0800

    test01

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    /**
+    转义字符：
+     \n --> 换行
+     \r --> 回车
+     \t --> tab 键
+     \" --> 转义符，如；直接输出 "" ,或者是上面三个
+
+
+     */
+    public static void main(String[] args) {
+//        String s = "\r";
+        String s = "\r\"";
+
+        System.out.println(s);
+    }
+
+}
