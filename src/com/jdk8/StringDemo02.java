65447747bfa15d5a701b8afa1c05716a2d13a9fe
kobe <1329989713@qq.com>
2019-06-12 16:37:29 +0800

    test01

---
+++ src/com/jdk8/StringDemo02.java
@@ -0,0 +1,30 @@
+package com.jdk8;
+
+/**
+
+
+ String 常用方法
+
+ charAt()
+ isEmpty()
+ indexOf()
+ lastIndexOf()
+ substring()
+ trim()
+ replace()
+ startWith()
+ endsWith()
+ split()
+ join()
+ valueOf()
+ compareTo()
+
+ */
+public class StringDemo02 {
+    public static void main(String[] args) {
+        String s = "abcdef";
+        System.out.println(s.charAt(0));
+        System.out.println(s.isEmpty());
+        System.out.println(s.indexOf(1));
+    }
+}
