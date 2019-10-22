993c308743b10cf6c9f59622e7d064f8246959b9
kobe1329989713 <1329989713@qq.com>
2019-10-22 13:58:41 +0800

    <T> T m(T t) 这种泛型。

---
+++ src/com/jdk8/toa3/generic/GenericTT.java
@@ -0,0 +1,45 @@
+package com.jdk8.toa3.generic;
+
+import java.util.List;
+
+/**
+ * Created by kobe on 2019/10/22.13:51
+ *
+ * @author kobe
+ * <br/>
+ * Description:
+ * <T> T表示返回值是一个泛型，传递啥 ，就返回啥类型的数据，
+ */
+public class GenericTT<T> {
+
+    /**
+     * <T> T 可以传入任何类型的List
+     * 参数T
+     * 第一个 表示是泛型 ****
+     * 第二个 表示返回的是T类型的数据
+     * 第三个 限制参数类型为T
+     *
+     * @param data z
+     * @return
+     */
+    private <T> T getListFirst(List<T> data) {
+        if (data == null || data.size() == 0) {
+            return null;
+        }
+        return data.get(0);
+    }
+
+    /**
+     * 这个只能传递T类型的数据
+     * 返回值 就是实例化传递的对象类型
+     *
+     * @param data
+     * @return
+     */
+    private T getListFisrt(List<T> data) {
+        if (data == null || data.size() == 0) {
+            return null;
+        }
+        return data.get(0);
+    }
+}
